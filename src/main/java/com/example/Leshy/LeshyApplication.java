94a1a809d5579e972bcd3fa63cecac6aeb990321
jacek-domanski <j.domanski92@gmail.com>
2021-05-19 19:38:39 +0200

    Initial project files

---
+++ src/main/java/com/example/Leshy/LeshyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Leshy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeshyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeshyApplication.class, args);
+	}
+
+}
