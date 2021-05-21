ab8bc1dd0a16637a2889cdb744a792f4ebb3abc6
jacek-domanski <j.domanski92@gmail.com>
2021-05-22 00:21:35 +0200

    Add PlantSingleSpecies class

---
+++ src/main/java/com/example/Leshy/PlantSingleSpecies.java
@@ -0,0 +1,29 @@
+package com.example.Leshy;
+
+public class PlantSingleSpecies {
+    private String name;
+    private int wateringFrequencyDays;
+    
+    public PlantSingleSpecies() {
+    }
+    
+    public PlantSingleSpecies(String name, int wateringFrequencyDays) {
+        this.name = name;
+        this.wateringFrequencyDays = wateringFrequencyDays;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getWateringFrequencyDays() {
+        return wateringFrequencyDays;
+    }
+    public void setWateringFrequencyDays(int wateringFrequencyDays) {
+        this.wateringFrequencyDays = wateringFrequencyDays;
+    }
+
+    
+
+}
